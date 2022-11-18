ec61f18e6601c504313700d769b97ff06a680338
MarcylleneMaria <marcyllenemaria@id.uff.br>
2022-11-18 16:17:34 -0300

    subindo codigo aula 7

---
+++ src/components/Lista.jsx
@@ -0,0 +1,25 @@
+// quando a tela carregar eu quero consumir uma API e mostrar os dados na tela
+import { useEffect, useState } from "react"
+import Axios from 'axios'
+
+export const Lista = () => {
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        async function pegaDados() {
+            const resposta = await Axios.get('https://pokeapi.co/api/v2/pokemon')
+            setPokemons(resposta.data.results)
+        }
+        pegaDados()
+    }, [])
+
+    return (
+        <>
+        {pokemons.map(pokemon =>  {
+        return(
+            <p key={pokemon.name}>{pokemon.name}</p>
+        )
+        })}
+        </>
+    )
+}
